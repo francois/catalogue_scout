7d85e6b7a369ba54d0dfc221c91728478d8bfbca
François Beausoleil <francois@teksol.info>
2018-02-15 09:35:03 -0500

    Prevent slugs from changing between updates
    Slugs must be permanent because they are accessible from the web-tier.
    Changing the URL of an object would be a problem.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -4,6 +4,8 @@ class ApplicationRecord < ActiveRecord::Base
   after_initialize {|r| r.clear_pending_event_log_records}
   after_initialize {|r| r.slug = r.class.generate_slug if r.attributes.include?("slug") && r.slug.blank? }
 
+  validate :slug_has_not_changed, on: :update
+
   attr_reader :pending_event_log_records
 
   def clear_pending_event_log_records
@@ -17,5 +19,12 @@ class ApplicationRecord < ActiveRecord::Base
 
   def self.generate_slug
     SecureRandom.base58(8).downcase
+  end
+
+  def slug_has_not_changed
+    return unless attributes.include?("slug")
+    return unless slug_changed?
+
+    errors.add(:slug, "must not change")
   end
 end
--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class GroupTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @group = Group.new(name: "10eme")
+  end
+
+  test "changing the slug of an existing object records an error" do
+    @group.save!
+    @group.slug = "fhj"
+    assert_equal false, @group.save
+    assert @group.errors.include?("slug")
+  end
 end
